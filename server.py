f781c7b627e34b105b829bb7c280f1cb792b3312
Michael Cruz <michael.cruz@uwalumni.com>
2017-07-25 21:33:50 -0700

    database schema

--- server.py
+++ server.py
@@ -4,6 +4,16 @@ import sqlite3
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
 
+# conn = sqlite3.connect('database.db')
+# c = conn.cursor()
+#
+# c.execute('CREATE TABLE elements (element_id INTEGER PRIMARY KEY, element_type TEXT NOT NULL, content TEXT NOT NULL, post_id INTEGER NOT NULL, FOREIGN KEY (post_id) REFERENCES posts(post_id))')
+#
+# c.execute('CREATE TABLE posts (post_id INTEGER PRIMARY KEY, post_date TEXT, title TEXT, subtitle TEXT)')
+
+# conn.commit()
+# conn.close()
+
 def read_data():
     print 'inside read_data() function'
     conn = sqlite3.connect('database.db')
@@ -20,23 +30,24 @@ def display():
 @app.route('/post')
 def weblog():
     post = {
-            "date": "kjsdck",
-            "title": ",m ,ms",
+            "post_date": "date",
+            "title": "title",
+            "subtitle": "subtitle",
             "elements": [
                 {
-                  "type": "text",
+                  "element_type": "text",
                   "content": "Sup, yo."
                 },
                 {
-                  "type": "snippet",
+                  "element_type": "snippet",
                   "content": "Hello, I am content"
                 },
                 {
-                  "type": "snippet",
+                  "element_type": "snippet",
                   "content": "and yup"
                 },
                 {
-                  "type": "snippet",
+                  "element_type": "snippet",
                   "content": "It was a good day!"
                 }
             ]
@@ -54,16 +65,18 @@ def query():
 
 @app.route('/data', methods=['POST'])
 def data():
-    date = flask.request.get_json()['post']['date']
-    title = flask.request.get_json()['post']['title']
-    elements = flask.request.get_json()['post']['elements']
-    post = (date, title, elements)
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO posts VALUES (?, ?, ?)", post)
-    conn.commit()
-    conn.close()
-    read_data()
+    date = flask.request.get_json(force=False)['post']['post_date']
+    print date
+    # title = flask.request.get_json()['post']['title']
+    # subtitle = flask.request.get_json()['post']['subtitle']
+    # elements = flask.request.get_json()['post']['elements']
+    # post = (date, title, elements)
+    # conn = sqlite3.connect('database.db')
+    # c = conn.cursor()
+    # c.execute("INSERT INTO posts VALUES (?, ?, ?)", post)
+    # conn.commit()
+    # conn.close()
+    # read_data()
     return flask.jsonify({})
 
 if __name__ == '__main__':
